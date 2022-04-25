8ecf3c5439b99dc216c67f66d0b5c67af2924d3e
AIFanatic <aifanatic@github.com>
2022-04-25 12:23:19 +0000

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 export * as THREE from 'three';
 
 export { Scene } from './Scene';
+export { Renderer } from './Renderer';
+export { Physics } from './Physics';
+
 export { Input } from './Input';
 export { KeyCodes } from './enums/KeyCodes';
 export { LayerMask } from './enums/LayerMask';
+export { HideFlags } from './enums/HideFlags';
+
 export { GameObject } from './components/GameObject';
 
 export * as Components from './components';
@@ -12,4 +17,10 @@ export { ArticulationJointType } from './enums/ArticulationJointType';
 export { ArticulationDofLock } from './enums/ArticulationDofLock';
 export { PrimitiveType } from './enums/PrimitiveType';
 
-export { Resources } from './Resources';
+export { ResourcesCache } from './resources/ResourcesCache';
+export { Resources } from './resources/Resources';
+
+export { SceneSerializer } from './serializer/SceneSerializer';
+export { SceneDeserializer } from './serializer/SceneDeserializer';
+
+export { SerializeField } from './utils/SerializeField';
